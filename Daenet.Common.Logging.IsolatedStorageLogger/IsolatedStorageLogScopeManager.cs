c5a66588f49a3b3772f88e93c27c45304935952a
Mofaggol Hoshen <summoncse@gmail.com>
2017-07-14 12:45:12 +0200

    Added IsolatedStoreageLogger and IsolatedStorageLogScopManageer class

---
+++ Daenet.Common.Logging.IsolatedStorageLogger/IsolatedStorageLogScopeManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daenet.Common.Logging.IsolatedStorageLogger
+{
+    internal class IsolatedStorageLogScopeManager
+    {
+        public IsolatedStorageLogScopeManager(object state)
+        {
+
+        }
+
+        internal IDisposable Push(object state)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Daenet.Common.Logging.IsolatedStorageLogger/IsolatedStorageLogger.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daenet.Common.Logging.IsolatedStorageLogger
+{
+    public class IsolatedStorageLogger : ILogger
+    {
+        private IsolatedStorageLogScopeManager m_ScopeManager;
+        private Func<string, LogLevel, bool> m_Filter;
+        private string m_CategoryName;
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            if(state == null)
+            {
+                throw new ArgumentNullException(nameof(state));
+            }
+
+            if(m_ScopeManager == null)
+            {
+                m_ScopeManager = new IsolatedStorageLogScopeManager(state);
+            }
+
+            var scope = m_ScopeManager.Push(state);
+
+            return scope;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return m_Filter(m_CategoryName, logLevel);
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
