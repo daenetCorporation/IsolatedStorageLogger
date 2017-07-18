341b111acb14fedcbb50729c91453642cda18b7b
Mofaggol Hoshen <summoncse@gmail.com>
2017-07-18 10:42:16 +0200

    Write scopemanager

--- Daenet.Common.Logging.IsolatedStorageLogger/IsolatedStorageLogScopeManager.cs
+++ Daenet.Common.Logging.IsolatedStorageLogger/IsolatedStorageLogScopeManager.cs
@@ -2,22 +2,84 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Daenet.Common.Logging.IsolatedStorageLogger
 {
     internal class IsolatedStorageLogScopeManager
     {
-        public IsolatedStorageLogScopeManager(object state)
-        {
+        internal static readonly AsyncLocal<List<DisposableScope>> m_AsyncSopes = new AsyncLocal<List<DisposableScope>>();
+        private object m_State;
 
+        public object Current
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var item in m_AsyncSopes.Value)
+                {
+                    sb.Append($"/{item}");
+                }
+
+                return sb.ToString();
+            }
         }
 
-        public object Current { get; internal set; }
+        public IsolatedStorageLogScopeManager(object state)
+        {
+            m_AsyncSopes.Value = new List<DisposableScope>();
+            m_State = state;
+        }
 
         internal IDisposable Push(object state)
         {
-            throw new NotImplementedException();
+            lock ("scope")
+            {
+                var newScope = new DisposableScope(state.ToString(), this);
+
+                m_AsyncSopes.Value.Add(newScope);
+
+                return newScope;
+            }
+        }
+        public override string ToString()
+        {
+            return m_State?.ToString();
+        }
+
+        internal class DisposableScope : IDisposable
+        {
+            #region Member Variables
+            private IsolatedStorageLogScopeManager m_ScopeMgr;
+            private string m_ScopeName;
+            #endregion
+
+            #region Public Methods
+            public DisposableScope(string scopeName, IsolatedStorageLogScopeManager scopeMgr)
+            {
+                this.m_ScopeMgr = scopeMgr;
+                this.m_ScopeName = scopeName;
+            }
+            public void Dispose()
+            {
+                lock ("scope")
+                {
+                    var me = m_AsyncSopes.Value.FirstOrDefault(s => s == this);
+                    if(me == null)
+                    {
+                        throw new InvalidOperationException("This sould never happen!");
+                    }
+
+                    m_AsyncSopes.Value.Remove(me);
+                }
+            }
+
+            public override string ToString()
+            {
+                return m_ScopeName;
+            }
+            #endregion
         }
     }
 }
